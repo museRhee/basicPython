e14752220dbd691aa57770a6fa8de7481ab8c203
museRhee <donggyu.rhee@gmail.com>
2017-09-08 10:52:33 +0900

    ageCheck

--- ageCheck.py
+++ ageCheck.py
@@ -1,4 +1,7 @@
-age = int(input('How old are U? '))
+age = int(input("How old are U? "))
 
 if (age>=20):
-    print('U are an adult')
+    print("U are an adult")
+else:
+    print("U are a baby")
+    
