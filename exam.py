fef837d0a98af0d58f96fa9d568a6ef9dd386598
museRhee <donggyu.rhee@gmail.com>
2017-09-08 13:42:30 +0900

    exam

--- exam.py
+++ exam.py
@@ -1,11 +1,15 @@
+#input subject score
 aScore = int(input("Score of A? "))
 bScore = int(input("Score of B? "))
 cScore = int(input("Score of C? "))
 
-avg = int((aScore + bScore + cScore) / 3)
+#average score
+avg = (aScore + bScore + cScore) / 3
 
+
+#print result
 if (aScore>=60 and bScore>=60 and cScore>=60) and (avg>=70):
-    print("The average is ", avg, " and passed", sep="")
+    print("The average is ", int(avg), " and passed", sep="")
 else:
-    print("The average is ", avg, " and failed", sep="")
+    print("The average is ", int(avg), " and failed", sep="")
     
