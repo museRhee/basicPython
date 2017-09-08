65c10e6c462b270aa9ca3639e1ea5d81437f9413
museRhee <donggyu.rhee@gmail.com>
2017-09-08 13:43:09 +0900

    funPark

--- funPark.py
+++ funPark.py
@@ -1,6 +1,8 @@
+#input child's height and weight
 height = int(input("child's height? "))
 weight = int(input("child's weight? "))
 
+#print result
 if (height<=155 and weight<=50):
     print("Can ride")
 else:
