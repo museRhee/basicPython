ebaedf593c7d884b96cc2a89617a500ebcad4ed0
museRhee <donggyu.rhee@gmail.com>
2017-09-14 16:25:09 +0900

    exam

--- exam.py
+++ exam.py
@@ -6,10 +6,17 @@ cScore = int(input("Score of C? "))
 #average score
 avg = (aScore + bScore + cScore) / 3
 
+#round off score
+avg = round(avg, 2)
 
-#print result
-if (aScore>=60 and bScore>=60 and cScore>=60) and (avg>=70):
-    print("The average is ", int(avg), " and passed", sep="")
-else:
-    print("The average is ", int(avg), " and failed", sep="")
-    
+#pass if each score over 60 and avg over 70
+if aScore < 60 :
+    print("failure_history score is " + str(aScore))
+elif bScore < 60 :
+    print("failure_general knowledge score is " + str(bScore))
+elif cScore < 60 :
+    print("failure_manners score is " + str(cScore))
+elif avg < 70 :
+    print("failure_average score is " + str(avg))
+else :
+    print("passed_average score is " + str(avg))
